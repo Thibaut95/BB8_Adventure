8d8c6a375f7dea6fce61825004547603dd1d77af
Thibaut <thibaut.piquerez@he-arc.ch>
2018-04-27 08:50:53 +0200

    ajout du prefab hud

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");     
+    }
+
+    public void restartLevel()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+        Time.timeScale = 1;
+    }
+
+}
