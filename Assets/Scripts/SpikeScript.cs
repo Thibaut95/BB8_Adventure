79bbee6a2234d7b1a4e5bc63a7951ef9dca5d6b3
Castella Killian <killian.castella@he-arc.ch>
2018-04-27 08:26:32 +0200

    spike script

---
+++ Assets/Scripts/SpikeScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeScript : MonoBehaviour {
+    // Use this for initialization
+    private Vector3 positionStart;
+    private Vector3 positionEnd;
+    private GameObject cone;
+    private int direction;
+
+
+    void Start () {
+        cone = transform.Find("Cone").gameObject;
+        positionEnd = cone.transform.position;
+        positionStart = cone.transform.position - new Vector3(0,1.7f,0);
+        cone.transform.position = positionStart;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        if (direction == 1)
+        {   
+            if(cone.transform.position.y < positionEnd.y)
+            {
+                cone.transform.position = cone.transform.position + new Vector3(0, 0.1f, 0);
+            }
+            else
+            {
+                direction = -1;
+            }
+        }else if (direction == -1)
+        {
+            if (cone.transform.position.y > positionStart.y)
+            {
+                cone.transform.position = cone.transform.position + new Vector3(0, -0.02f, 0);
+            }
+            else
+            {
+                direction = 0;
+            }
+        }
+        
+	}
+    void OnCollisionEnter(Collision collision)
+    {
+        direction = 1;
+    }
+}
